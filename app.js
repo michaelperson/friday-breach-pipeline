567a1ce931389eb1bf8b35e43159d6a095a3b5e8
PERSON <michael.person@cognitic.be>
2026-04-17 16:05:54 +0200

    Add basic addition and greeting functions

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js — Notre "application" ultra-simple
+
+function additionner(a, b) {
+
+  return a + b;
+
+}
+
+function direBonjour(nom) {
+
+  if (!nom) throw new Error("Un nom est requis !");
+
+  return `Bonjour, ${nom} !`;
+
+}
+
+module.exports = { additionner, direBonjour };
