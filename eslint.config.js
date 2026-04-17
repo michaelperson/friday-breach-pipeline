835c5d372169beacbc4b83e2f55b1c783e3f8070
michael person <michael.person@cognitic.be>
2026-04-17 17:18:34 +0200

    chore: fix ESLint config (CommonJS, Node/Jest globals)

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,34 @@
-export default [
+module.exports = [
   {
-    files: ["**/*.js"],
+    files: ["**/*.js", "!eslint.config.js"],
     languageOptions: {
       ecmaVersion: "latest",
-      sourceType: "module"
+      sourceType: "module",
+      globals: {
+        module: "writable",
+        require: "readonly",
+        exports: "writable",
+        __dirname: "readonly",
+        __filename: "readonly"
+      }
     },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error"
+    }
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        jest: "readonly"
+      }
+    },
+    rules: {
+      "no-undef": "off"
     }
   }
 ];
